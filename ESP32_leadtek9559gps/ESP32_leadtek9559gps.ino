38238b0d95f61936539abdbbd90b94bd4ffac225
xiaolaba <xiao_laba_cn@yahoo.com>
2022-12-01 23:20:15 +0800

    Update ESP32_leadtek9559gps.ino

--- ESP32_leadtek9559gps/ESP32_leadtek9559gps.ino
+++ ESP32_leadtek9559gps/ESP32_leadtek9559gps.ino
@@ -1,4 +1,7 @@
-// https://esp32.com/viewtopic.php?t=7889
+// mod, by xiaolaba, https://github.com/xiaolaba/GPS_leadtek_9559X_ESP32
+
+
+// ref: https://esp32.com/viewtopic.php?t=7889
 
 //This example code is in the Public Domain (or CC0 licensed, at your option.)
 //By Victor Tchistiak - 2019
@@ -16,18 +19,19 @@ BluetoothSerial SerialBT;
 //uint8_t address[6]  = {0xAA, 0xBB, 0xCC, 0x11, 0x22, 0x33};
 
 
-// my leadtek 9559 GPS MAC address
+// my leadtek 9559 series BT GPS, MAC address
+// 00:02:78:14:0b:fc
 uint8_t address[6] = {0x00, 0x02, 0x78, 0x14, 0x0B, 0xFC};
 String name = "Leadtek 9559 series BT GPS";
-char *pin = "0000"; //<- standard pin would be provided by default
+char *pin = "0000"; // Bluetooth device connecting pin#
 
 bool connected;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32test", true);
+  SerialBT.begin("ESP32 BT terminal for GPS test", true);
   SerialBT.setPin(pin);
-  Serial.println("The device started in master mode, make sure remote BT device is on!");
+  Serial.println("ESP32 started in master mode, make sure Leadtek 9559 series BT GPS is on & blue LED blinking! Once BT connected, LED is lite");
   
   // connect(address) is fast (upto 10 secs max), connect(name) is slow (upto 30 secs max) as it needs
   // to resolve name to address first, but it allows to connect to different devices with the same name.
@@ -42,7 +46,7 @@ void setup() {
     Serial.println("Connected Succesfully!");
   } else {
     while(!SerialBT.connected(10000)) {
-      Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app.");
+      Serial.println("Failed to connect. power off/on bluetooth GPS, then reset ESP32.");
     }
   }
   
